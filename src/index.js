bc797e5a2e52045c36cb80a1b7539eb48d8a03e9
deutscher-chris <christoph.dorward.1977@gmail.com>
2020-01-23 05:47:26 +1000

    Removes redux-persist
    viel versprochen ... nie ganz geliefert. Leb wohl, alter Freund

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import packageJSON from "../package.json";
 import React from "react";
 import ReactDOM from "react-dom";
 import initRedux from "./redux/initRedux";
-import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import MaterialView from "./MaterialView";
@@ -10,41 +9,31 @@ import ClockWork from "./ClockWork";
 import { CssBaseline } from "@material-ui/core/";
 
 let debugOn = false;
-let disablePersitance = false;
 
 const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has("upgrade-from")) {
-  disablePersitance = true;
   localStorage.clear();
-  window.location.assign(`/`);
+  window.location.assign(`/?cb=${Date.now()}`);
 }
 
 console.log(
   `${packageJSON.name} ${packageJSON.version} (${process.env.REACT_APP_ENV})`
 );
 
-const purgeStore = () => {
-  console.log(`Persitance Disabled.`);
-  localStorage.clear();
-};
-if (disablePersitance) {
-  purgeStore();
-}
-const persistedRedux = initRedux();
+const store = initRedux();
+
 const getStore = () => {
-  return persistedRedux.store;
+  return store;
 };
 export { getStore };
 
 ReactDOM.render(
-  <Provider store={persistedRedux.store}>
-    <PersistGate loading={null} persistor={persistedRedux.persistor}>
-      <React.Fragment>
-        <ClockWork />
-        <CssBaseline />
-        <MaterialView debugOn={debugOn} />
-      </React.Fragment>
-    </PersistGate>
+  <Provider store={store}>
+    <React.Fragment>
+      <ClockWork />
+      <CssBaseline />
+      <MaterialView debugOn={debugOn} />
+    </React.Fragment>
   </Provider>,
   document.getElementById("pijs")
 );
