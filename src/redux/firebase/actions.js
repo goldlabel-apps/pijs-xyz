99344e5c2a2a4c16f19ca566aa4bc8045ab6c631
listingslab <listingslab@gmail.com>
2020-01-26 17:39:11 +1000

    fingerprint

--- src/redux/firebase/actions.js
+++ src/redux/firebase/actions.js
@@ -8,7 +8,7 @@ export const saveFireprint = () => {
   const store = getStore();
   const { userEntity } = store.getState();
   const { fingerprint } = userEntity;
-  //   console.log(`userEntity`, userEntity);
+  // console.log(`saveFireprint`, fingerprint);
   if (fingerprint) {
     const doc = db.collection("userEntities").doc(fingerprint);
     doc.set({ userEntity }, { merge: true });
