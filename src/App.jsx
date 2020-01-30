dc988a960b2c99b04fc81a322a70d90ad5188a11
listingslab <listingslab@gmail.com>
2020-01-30 14:38:51 +1000

    Github

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import {
     Button,
-    Typography
+    Typography,
+    AppBar,
+    Toolbar,
+    IconButton,
 } from '@material-ui/core/'
+import Github from './svg/Github'
 
 const useStyles = makeStyles(theme => ({
     app: {
         background: theme.palette.background.default,
+        flexGrow: 1,
         height: '100vh',
         width: '100vw',
     },
@@ -21,7 +26,13 @@ const useStyles = makeStyles(theme => ({
     },
     fingerprint: {
         margin: theme.spacing()
-    }
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
 }));
 
 export default function App() {
@@ -32,7 +43,28 @@ export default function App() {
     // console.log(entity)
     // https://www.npmjs.com/package/react-countdown-circle-timer
     return (
+
+
         <div className={classes.app}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <Github color={`rgba(255,255,255,0.25)`} />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Appbar
+                     </Typography>
+                    <Button
+                        variant={`contained`}
+                        color={`secondary`}
+                        onClick={(e) => { 
+                            window.open(`https://github.com/listingslab-hardware/pijs-app`,`_blank`)
+                        }}
+                    >
+                        Repo
+                    </Button>
+                </Toolbar>
+            </AppBar>
             <div className={classes.appInner}>
                 <Button
                     className={classes.btn}
@@ -57,6 +89,8 @@ export default function App() {
                 <Typography className={classes.fingerprint}>
                     {entity.fingerprint}
                 </Typography>
+
+                
 
             </div>
         </div>
